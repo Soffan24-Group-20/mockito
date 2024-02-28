6b8df30ec8321edb4788bb668b822a8259316aec
Fredrik Blomqvist <104637174+frblo@users.noreply.github.com>
2024-02-28 15:49:10 +0100

    Fix #27 (#28)
    * add skeleton
    
    * Add initial tests

---
+++ src/main/java/org/mockito/internal/matchers/ListAnyOrder.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2007 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+package org.mockito.internal.matchers;
+
+import java.util.Collection;
+import org.mockito.ArgumentMatcher;
+
+public class ListAnyOrder<T> implements ArgumentMatcher<Collection<T>>{
+    private final Collection<T> expected;
+
+    public ListAnyOrder(Collection<T> expected) {
+        this.expected = expected;
+    }
+
+    @Override
+    public boolean matches(Collection<T> actual) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'matches'");
+    }
+
+    public <E> boolean deepMatches(Collection<E> actual) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deepMatches'");
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+    }    
+}
